af8f52574ba11d51e2c96f8adfacb8a8a97c88b8
Brian <brian@itdev-lappy>
2016-06-14 19:46:53 -0500

    Basic Framework of our Shopping List WebApp.

---
+++ node/shoppinglist.js
@@ -0,0 +1,16 @@
+//Main JS file of our shoppinglist app
+
+var express = require('express'); 	//Import our express module
+
+var app = express(); 				//create our app object that holds our routes
+
+app.listen(3000, function(err){		//Start listening on port 3000
+	console.log('shoppinglist listening on http://localhost:3000');
+		});
+
+app.use('/',express.static('../app'));	//Serve our static HTML files //periods go up one directory //where you have to navigate to to get the index.html file
+//reads "App mounted at route"
+
+app.get('/list', function(req, res) {
+		res.json(['Cat Food', 'Toothpaste', 'Bananas']);
+});
